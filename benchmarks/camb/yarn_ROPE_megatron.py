12e9b5d8e5a9a176d3f6f609b1900427f8581d81
geruijun <geruijun@pjlab.org.cn>
2024-12-09 15:46:42 +0800

    add yarn ROPE

---
+++ benchmarks/camb/yarn_ROPE_megatron.py
@@ -0,0 +1,101 @@
+import time
+from typing import List
+import torch
+import torch_mlu
+from torch_mlu.utils.model_transfer import transfer
+
+from torch import nn
+import torch.nn.functional as F
+from torch.profiler import profile, record_function, ProfilerActivity
+import triton
+import dlblas
+from dlblas.utils.device_utils import get_idle_device
+
+from functorch.compile import aot_module, make_boxed_func, aot_function
+from torch._dynamo.backends.common import aot_autograd
+
+device_ = torch.device(get_idle_device())
+torch.cuda.set_device(device_)
+
+def yarn_ROPE(max_seq_len, offset, inv_freq):
+    seq = (
+        torch.arange(
+            max_seq_len, device=inv_freq.device, dtype=inv_freq.dtype
+        )
+        + offset
+    )
+
+    freqs = torch.outer(seq, inv_freq) 
+
+    emb = torch.cat((freqs, freqs), dim=-1)
+    # # emb [seq_length, .., dim]
+    return emb
+
+def test():
+    max_seq_len = 4096
+    q_head_dim = 32
+    offset = 0
+    inv_freq = torch.arange(0, q_head_dim, dtype=torch.float32, device=device_)
+
+    with torch.no_grad():
+        inv_freq_tri = (
+            inv_freq.clone()
+        )
+    inv_freq.requires_grad = True
+    inv_freq_tri.requires_grad = True
+
+    emb = yarn_ROPE(max_seq_len, offset,inv_freq)
+    loss_torch = torch.sum(torch.mean(emb))
+    loss_torch.backward(retain_graph=True)
+
+    from dlblas.kernels.camb.yarn_ROPE import yarnROPE
+    emb_triton = yarnROPE.apply(max_seq_len, offset, inv_freq_tri)
+ 
+    print(f"out_q max diff: {(emb - emb_triton).abs().max().item()}")
+    assert torch.allclose(emb, emb_triton)
+
+    configs = []
+    configs.append(
+        triton.testing.Benchmark(
+            x_names=["op"],
+            x_vals=["fwd"],
+            line_arg="provider",
+            line_vals=["triton", "pytorch"],
+            line_names=["Triton", "PyTorch"],
+            #line_vals=["pytorch"],
+            #line_names=["PyTorch"],
+            ylabel="ms",
+            plot_name=f"yarn_ROPE",
+            args={"q_head_dim": q_head_dim},
+        )
+    )
+
+    @triton.testing.perf_report(configs)
+    def bench_fn(q_head_dim, op, provider, device=device_):
+        warmup = 100
+        rep = 200
+
+        if "triton" in provider:
+            if "fwd" == op:
+                fn = lambda: yarnROPE.apply(
+                    max_seq_len, offset, inv_freq
+                )
+
+        if "pytorch" in provider:
+            if "fwd" == op:
+                fn = lambda: yarn_ROPE(max_seq_len, offset, inv_freq)
+            # elif "bwd" == op:
+            #     out_q, out_k = partial_rotary_emb(q, k_pe, k_nope, cos, sin)
+            #     loss_torch = torch.sum(torch.mean(out_q) * torch.mean(out_k))
+            #     fn = lambda: loss_torch.backward(retain_graph=True)
+            # else:
+            #     raise Exception()
+        ms = triton.testing.do_bench(fn, warmup=warmup, rep=rep)
+        return ms
+
+    bench_fn.run(show_plots=True, print_data=True)
+
+
+if __name__ == "__main__":
+    test()
+    print("sucessfully!")
---
+++ dlblas/kernels/camb/yarn_ROPE.py
@@ -0,0 +1,68 @@
+import torch
+import triton
+import triton.language as tl
+from dlblas.utils.libentry import libentry
+from dlblas.utils import register_dlblas_op, SymVar, Tensor, ChoiceSpace
+
+@libentry()
+@triton.autotune(
+    configs=[
+        triton.Config({"BLOCK_SEQ": BS}, num_stages=s, num_warps=w)
+        for BS in [1024]
+        for s in [4]
+        for w in [4]
+    ],
+    key=["max_seq_len"],
+)
+
+@triton.jit
+def yarnROPE_fwd_kernel(
+    inv_freq,
+    max_seq_len,
+    offset,
+    half_head_dim: tl.constexpr,
+    emb,
+    BLOCK_SEQ: tl.constexpr,
+):
+    seq_idx = tl.program_id(0)
+    offs_seq = seq_idx * BLOCK_SEQ + tl.arange(0, BLOCK_SEQ)
+    cur_seq_data = (seq_idx * BLOCK_SEQ + tl.arange(0,BLOCK_SEQ)).view(BLOCK_SEQ,1)
+    if offset != 0:
+        cur_seq_data += offset
+    inv_freq_data = tl.load(inv_freq + tl.arange(0,half_head_dim)).view(1,half_head_dim)
+
+    cur_emb_data = cur_seq_data * inv_freq_data
+
+    cur_emb_data_offset0 = offs_seq[:,None] * half_head_dim * 2 + tl.arange(0,half_head_dim)[None,:]
+
+    cur_emb_data_offset1 = cur_emb_data_offset0 + half_head_dim
+
+    tl.store(emb + cur_emb_data_offset0, cur_emb_data)
+    tl.store(emb + cur_emb_data_offset1, cur_emb_data)
+
+class yarnROPE(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx: torch.Any, max_seq_len, offset, inv_freq):
+        half_head_dim = inv_freq.shape[0]
+        emb = torch.empty(max_seq_len, half_head_dim * 2, device='mlu')
+
+        assert (
+            inv_freq.is_contiguous()
+        )
+
+        with torch.cuda.device(inv_freq.device):
+            grid = lambda META: (
+                triton.cdiv(max_seq_len, META["BLOCK_SEQ"]),
+            )
+            yarnROPE_fwd_kernel[grid](
+                inv_freq,
+                max_seq_len,
+                offset,
+                half_head_dim,
+                emb,
+            )
+        return emb
+
+
+
+

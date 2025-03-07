c1b4b9b659c67526496e83c31842637a8b48156d
Pan Daoxin <pandaoxin@pjlab.org.cn>
2025-03-07 06:30:51 +0000

    Add e5m2 to e4m3 conversion.

---
+++ dlblas/kernels/cutlass/fp8_convert.cu
@@ -0,0 +1,15 @@
+#include <cuda.h>
+#include <cuda_fp8.h>
+#include <iostream>
+
+#include "fp8_convert.cuh"
+
+extern "C" void launch(void* __raw_in, void* __raw_out) {
+    auto in = reinterpret_cast<cutlass::float_e5m2_t*>(__raw_in);
+    auto out = reinterpret_cast<cutlass::float_e4m3_t*>(__raw_out);
+    cutlass::float_e5m2_t tmp_in;
+    cutlass::float_e4m3_t tmp_out;
+    cudaMemcpy(&tmp_in, in, 8, cudaMemcpyHostToHost);
+    convert(tmp_out, tmp_in);
+    cudaMemcpy(out, &tmp_out, 8, cudaMemcpyHostToHost);
+}
---
+++ tests/kernels/cutlass/test_fp8_convert.py
@@ -0,0 +1,30 @@
+import ctypes
+import os
+import torch
+
+from typing import Any, Dict
+
+ctype_map: Dict[Any, Any] = {
+    **{t: getattr(ctypes, f'c_{t.__name__}') for t in (bool, int, float)},
+    **{t: ctypes.c_void_p for t in (torch.int, torch.float, torch.bfloat16, torch.float8_e4m3fn, torch.float8_e5m2, torch.cuda.Stream)},
+}
+
+def map_ctype(value: Any) -> Any:
+    ctype = ctype_map[value.dtype if isinstance(value, torch.Tensor) else type(value)]
+    if isinstance(value, torch.Tensor):
+        return ctype(value.data_ptr())
+    if isinstance(value, torch.cuda.Stream):
+        return ctype(value.cuda_stream)
+    return ctype(value)
+
+class TestFp8Convert:
+
+    def test_fp8_convert(self):
+        os.system('git clone git@github.com:NVIDIA/cutlass.git && nvcc ../../../dlblas/kernels/cutlass/fp8_convert.cu -I./cutlass/include -shared --compiler-options=-fPIC -o ../../../dlblas/kernels/cutlass/fp8_convert.so')
+
+        lib = ctypes.CDLL('../../../dlblas/kernels/cutlass/fp8_convert.so')
+        inp = torch.rand(1).to(torch.float8_e5m2)
+        out = torch.zeros(1, device='cuda')
+        out = out.to(torch.float8_e4m3fn)
+        lib.launch(map_ctype(inp.cuda()), map_ctype(out))
+        assert torch.allclose(inp.to(torch.float), out.to(torch.float).cpu(), atol=1, rtol=0.01)

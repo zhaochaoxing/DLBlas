420227014aa4523824e2c519766d34b168d3bd6e
zhaochaoxing <zhaochaoxing163@163.com>
2025-10-14 10:07:28 +0000

    fix for npu

--- dlblas/kernels/lighting_indexer.py
+++ dlblas/kernels/lighting_indexer.py
@@ -14,6 +14,7 @@ def _kernel(
     seq_len_kv,
     heads: tl.constexpr,
     dim: tl.constexpr,
+    FP_8: tl.constexpr,
     BLOCK_Q: tl.constexpr,
     BLOCK_N: tl.constexpr,
 ):
@@ -36,11 +37,13 @@ def _kernel(
     for nbn_i in tl.range(0, tl.cdiv(cu_k_e_max - cu_k_s_min, BLOCK_N)):
     # k [BN, dim]   q[bq * heads, dim] = s [BN, bq * heads]
         k = tl.load(K + cu_k_s_min + nbn_i * BLOCK_N + block_n_offs[:,None] + dim_offs)
-        k_scale = tl.load(KScale + cu_k_s_min + nbn_i * BLOCK_N + block_n_offs)
-
         accumulator = tl.dot(k, q_t)
         s = tl.where(accumulator > 0, accumulator, 0)
-        s_reshaped = s * w * tl.broadcast_to(k_scale.reshape(BLOCK_N, 1), (BLOCK_N, BLOCK_Q * heads))
+        s_reshaped = s * w
+        if FP_8 == True:
+            k_scale = tl.load(KScale + cu_k_s_min + nbn_i * BLOCK_N + block_n_offs)
+            s_reshaped *= tl.broadcast_to(k_scale.reshape(BLOCK_N, 1), (BLOCK_N, BLOCK_Q * heads))
+
         out = tl.sum(s_reshaped.reshape(BLOCK_N, BLOCK_Q, heads), axis=2)
         out_offs = seq_len_offs[:, None] + cu_k_s_min + nbn_i * BLOCK_N + block_n_offs[None, :]
         tl.store(Logits + out_offs, tl.trans(out))
@@ -63,6 +66,7 @@ def lighting_indexer(q, kv, kv_scales, logits, weights, cu_seqlen_ks, cu_seqlen_
         seq_len_kv=seq_len_kv,
         heads=heads,
         dim = index_dim,
+        FP_8 = (kv_scales is not None),
         BLOCK_Q=BLOCK_Q,
         BLOCK_N=BLOCK_N,
     )
@@ -74,9 +78,7 @@ def mqa_attn_return_logits_interface(q,
                                      kv_scales,
                                      weights,
                                      cu_seqlen_ks,
-                                     cu_seqlen_ke,
-                                     clean_logits=True,
-                                     use_triton=True):
+                                     cu_seqlen_ke):
     seq_len, heads, index_dim = q.shape
     seq_len_kv = kv.shape[0]
     logits = torch.empty([seq_len, seq_len_kv], device=q.device, dtype=torch.float32)

d23a8f1267fbb4e0b4100f3ee3f19072ca11b897
zhaochaoxing <109726331+zhaochaoxing@users.noreply.github.com>
2026-01-27 15:19:23 +0800

    Update ci_python_black.sh

--- scripts/ci_python_black.sh
+++ scripts/ci_python_black.sh
@@ -12,5 +12,5 @@ echo "Found modified Python files for PR:"
 echo "$modified_py_files" | tr '\n' ' '  # 显示修改的文件列表
 
 # 使用 Black 检查这些文件（只检查，不修改）
-pip install black
+pip install black==25.1.0
 black --check --diff $modified_py_files
